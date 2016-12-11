c3c112b0d2978ff6dcce2a55c7a19d1e94005515
AjaxXu <xu_soft39211215@163.com>
2016-12-11 16:17:50 +0800

    first edit

--- server.py
+++ server.py
@@ -18,7 +18,6 @@ class IndexHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     @tornado.gen.engine
     def get(self):
-        query = self.get_argument('q')
         client = tornado.httpclient.AsyncHTTPClient()
         response = yield tornado.gen.Task(client.fetch,"http://m.baidu.com/api?action=search&word=*&apilevel=18&bdi_bear=WF&bdi_cid=9177265119920&bdi_imei=XWvGSz2%252F6IEu3xlWyyVa%252BQ%253D%253D&bdi_imsi=NWEzYjI4N2YyYjEzYmVmOA%3D%3D&bdi_loc=5YyX5Lqs5biC&bdi_mac=YWM6YmM6MzI6OWE6YmY6MzM%3D&bdi_uip=127.0.0.1&brand=OPSSON&cname=WS%3AYY&cpack=monkey&ct=1452249585&cver=&docid=9403459&dpi=300&from=1018654f&model=Q1&os_version=4.3&pver=3&refer_tag=rec&req_biz=1&resolution=720_1280&show_time=1452249585&token=jingyan&type=app&ua=opera&uid=712ACBBE63076AEC76BE860AQDEWE880&sign=CAC16505BC1964A2C4310707AD395DB4&action=appdetail&format=json")
         body = json.loads(response.body)
